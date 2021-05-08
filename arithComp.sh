c974701cee4cdd6bcbda7736641c3909fe90b9a9
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-05-08 18:50:07 +0530

    third computation done

--- arithComp.sh
+++ arithComp.sh
@@ -9,3 +9,5 @@ read -p "Enter the third number : " c
 result1=$(echo $a $b $c | awk '{print $1+$2*$3}')
 
 result2=$(echo $a $b $c | awk '{print $3+$1/$2}')
+
+result1=$(echo $a $b $c | awk '{print $1*$2+$3}')
